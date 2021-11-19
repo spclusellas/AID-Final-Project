ede9dc49e31e775a625434461b686ae03885b692
SPC <spclusellas@gmail.com>
2021-11-19 09:45:19 +0100

    main classes creation

--- sketch.js
+++ sketch.js
@@ -1,147 +1,233 @@
 let video
 let poseNet
-let leftWristPos
-let rightWristPos
 let poses = []
-let song = 0
-let text_song = "La valse d'Amélie"
-var piano = {
-	vals: undefined,
-	classic: undefined,
-	volume: 0,
-	currentTime: 0,
-}
-var violin = {
-	vals: undefined,
-	classic: undefined,
-	volume: 0,
-	currentTime: 0,
-}
-var guitar = {
-	vals: undefined,
-	classic: undefined,
-	volume: 0,
-	currentTime: 0,
+// Wrist variables
+let leftWrist, rightWrist
+// Instrument variables
+let piano, guitar, violin
+// Songs variables
+let pianoVals, pianoClassic, violinVals, violinClassic, guitarVals, guitarClassic
+//Instrument images
+let guitarImage, pianoImage, violinImage
+// Target Areas variables
+let targetArea1, targetArea2, targetArea3, targetArea4
+// CONFIDENCE WRIST DETECTION
+const CONFIDENCE_THRESHOLD = 0.5
+
+// CLASSES
+
+class Game {
+	constructor(start) {
+		this.start = start
+	}
 }
 
-var button
-let volume_left
-let volume_right
-let boolean_guitar = 0
-let boolean_violin = 0
-let boolean_piano = 0
-let guitarImage
-let pianoImage
-let violinImage
-let entro
-let r_hand = 0
-let l_hand = 0 // we need to determine which hand activates each instrument
-const CONFIDENCE_THRESHOLD = 0.3
+class Instrument {
+	constructor(vals, classic, volume, threshold = { bottom, top }, areaNumber) {
+		this.vals = vals
+		this.classic = classic
+		this.volume = volume
+		this.currentTime = 0
+		this.isPlaying = false
+		this.threshold = threshold
+		this.areaNumber = areaNumber
+	}
+
+	setVolume(volume) {
+		this.volume = volume
+	}
+
+	isPlayingSong() {
+		return this.classic.isPlaying() || this.vals.isPlaying()
+	}
+
+	playVals() {
+		this.vals.play()
+		this.vals.setVolume(this.volume)
+	}
+
+	stopVals() {
+		this.vals.stop()
+	}
+
+	playClassic() {
+		this.classic.play()
+		this.classic.setVolume(this.volume)
+	}
+
+	stopClassic() {
+		this.classic.stop()
+	}
+
+	isInActiveArea(xCoord) {
+		return xCoord >= this.threshold.bottom && xCoord <= this.threshold.top
+	}
+}
+
+class Wrist {
+	constructor(xCoord, yCoord, confidence) {
+		this.xCoord = xCoord
+		this.yCoord = yCoord
+		this.confidence = confidence
+		this.isActive = this.confidence >= CONFIDENCE_THRESHOLD
+	}
+
+	getActiveArea() {
+		return this.activeArea
+	}
+
+	getIsActive() {
+		return this.isActive
+	}
+
+	getXCoord() {
+		return this.xCoord
+	}
+
+	setXCoord(xCoord) {
+		this.xCoord = xCoord
+	}
+
+	setYCoord(yCoord) {
+		this.yCoord = yCoord
+	}
+
+	setIsActive(isActive) {
+		this.isActive = isActive
+	}
+
+	getTargetArea(targetAreas) {
+		let overArea = 0
+		targetAreas.forEach(targetArea => {
+			if (targetArea.isOverTargetArea(this)) {
+				overArea = targetArea.areaNumber
+			}
+		})
+		console.log(overArea)
+		return overArea
+	}
+
+	mapPositionToVolume(height) {
+		let volumeWrist
+
+		volumeWrist = 1 - Math.round10(this.yCoord / height, -2)
+
+		if (volumeWrist < 0) {
+			return 0
+		} else if (volumeWrist > 1) {
+			return 1
+		}
+
+		return volumeWrist
+	}
+
+	display() {
+		if (this.confidence >= CONFIDENCE_THRESHOLD) {
+			fill(0, 255, 0, 100)
+			ellipse(this.xCoord, this.yCoord, 40, 40)
+		}
+	}
+}
+
+class WristDetector {
+	constructor(diameter, xPosition, yPosition, wrist) {
+		this.diameter = diameter
+		this.xPosition = xPosition
+		this.yPosition = yPosition
+		this.wrist = wrist
+	}
+
+	setCoordinates(xPosition, yPosition) {
+		this.xPosition = xPosition
+		this.yPosition = yPosition
+	}
+
+	display(xPosition, yPosition) {
+		this.setCoordinates(xPosition, yPosition)
+		ellipse(this.xPosition, this.yPosition, this.diameter)
+	}
+}
+
+class TargetArea {
+	constructor(xCoord, yCoord, width, height, color, areaNumber) {
+		this.xCoord = xCoord
+		this.yCoord = yCoord
+		this.width = width
+		this.height = height
+		this.color = color
+		this.areaNumber = areaNumber
+	}
+
+	isOverTargetArea(wrist) {
+		return (
+			wrist.xCoord > this.xCoord &&
+			wrist.xCoord < this.xCoord + this.width &&
+			wrist.yCoord > this.yCoord &&
+			wrist.yCoord < this.yCoord + this.height
+		)
+	}
+
+	display() {
+		fill(this.color)
+		rect(this.xCoord, this.yCoord, this.width, this.height)
+	}
+}
 
 // all files needed to be loaded are in preload funciton
 function preload() {
 	guitarImage = loadImage('./images/guitar.png')
 	pianoImage = loadImage('./images/piano.png')
 	violinImage = loadImage('./images/violin.jpeg')
-	piano = {
-		...piano,
-		vals: loadSound('./music/piano_vals.mp3'),
-		classic: loadSound('./music/piano_chopin.mp3'),
-	}
-	guitar = {
-		...guitar,
-		vals: loadSound('./music/guitar_vals.mp3'),
-		classic: loadSound('./music/guitar_chopin.mp3'),
-	}
-	violin = {
-		...violin,
-		vals: loadSound('./music/violin_vals.mp3'),
-		classic: loadSound('./music/violin_chopin.mp3'),
-	}
-}
-
-function setImages() {
-	image(guitarImage, 56, 10, 80, 80)
-	image(violinImage, 270, 10, 80, 80)
-	image(pianoImage, 483, 10, 80, 80)
+	pianoVals = loadSound('./music/piano_vals.mp3')
+	pianoClassic = loadSound('./music/piano_chopin.mp3')
+	guitarVals = loadSound('./music/guitar_vals.mp3')
+	guitarClassic = loadSound('./music/guitar_chopin.mp3')
+	violinVals = loadSound('./music/violin_vals.mp3')
+	violinClassic = loadSound('./music/violin_chopin.mp3')
 }
 
 function setup() {
 	createCanvas(640, 580)
 	video = createCapture(VIDEO)
 	video.size(width, 480)
-	setImages()
+
 	translate(video.width, 0)
 	scale(-1, 1)
 	image(video, 0, 0, video.width, video.height)
-
+	// Create instrument instances
+	piano = new Instrument(pianoVals, pianoClassic, 0, { bottom: 0, top: 213 }, 1)
+	violin = new Instrument(violinVals, violinClassic, 0, { bottom: 213, top: 426 }, 2)
+	guitar = new Instrument(guitarVals, guitarClassic, 0, { bottom: 426, top: 640 }, 3)
 	// Create a new poseNet method with a single detection
 	poseNet = ml5.poseNet(video, modelReady)
 	// This sets up an event that fills the global variable "poses"
 	// with an array every time new poses are detected
-	poseNet.on('pose', function (results) {
-		if (results.length > 0) {
-			poses = results
-			leftWristPos = poses[0].pose.leftWrist
-			rightWristPos = poses[0].pose.rightWrist
+	poseNet.on('pose', function (poses) {
+		if (poses.length > 0) {
+			// Create Wrists instances in case poses are detected
+			leftWrist = new Wrist(poses[0].pose.leftWrist.x, poses[0].pose.leftWrist.y, poses[0].pose.leftWrist.confidence)
+			rightWrist = new Wrist(
+				poses[0].pose.rightWrist.x,
+				poses[0].pose.rightWrist.y,
+				poses[0].pose.rightWrist.confidence
+			)
+
+			// setHandDetection()
+			// WhatInstrumentSounds()
 		}
 	})
-	console.log(piano.classic.isLoaded())
-	// piano = new Audio('./music/piano_vals.mp3')
-	// guitar = new Audio('./music/guitar_vals.mp3')
-	// violin = new Audio('./music/violin_vals.mp3')
-
-	piano.volume = 0
-	guitar.volume = 0
-	violin.volume = 0
-
 	button = createButton('Songs')
 	button.style('background-color', color(200, 255, 255))
-	button.mousePressed(ChangeSong)
 
 	// Hide the video element, and just show the canvas
 	video.hide()
 }
 
-function ChangeSong() {
-	song = song + 1
-	if (song >= 2) {
-		song = 0
-	}
-	if (song == 0) {
-		text_song = "La valse d'Amélie"
-		piano.vals.play()
-		guitar.vals.play()
-		violin.vals.play()
-		piano.classic.pause()
-		guitar.classic.pause()
-		violin.classic.pause()
-		// piano.pause()
-		// guitar.pause()
-		// violin.pause()
-		//text(text_song,posX,posY);
-
-		// piano = new Audio('./music/piano_vals.mp3')
-		// guitar = new Audio('./music/guitar_vals.mp3')
-		// violin = new Audio('./music/violin_vals.mp3')
-	} else if (song == 1) {
-		text_song = 'Nocturne Op. 9 No. 2'
-		piano.classic.play()
-		guitar.classic.play()
-		violin.classic.play()
-		piano.vals.pause()
-		guitar.vals.pause()
-		violin.vals.pause()
-		// piano.pause()
-		// guitar.pause()
-		// violin.pause()
-		//text(text_song,posX,posY);
-
-		// piano = new Audio('./music/piano_chopin.mp3')
-		// guitar = new Audio('./music/guitar_chopin.mp3')
-		// violin = new Audio('./music/violin_chopin.mp3')
-	}
-	//to reset sketch: window.location.reload()
+function setupReady() {
+	if (typeof targetArea4 === 'undefined') return false
+	if (typeof leftWrist === 'undefined') return false
+	if (typeof rightWrist === 'undefined') return false
+	return true
 }
 
 function draw() {
@@ -150,139 +236,145 @@ function draw() {
 	image(video, 0, 100, width, 480)
 
 	// We can call both functions to draw all keypoints and the skeletons
-	if (poses.length > 0) {
-		drawKeypoints() //draw points in the hands
-		WhatInstrumentSounds() //tells which instruments are about to sound, and the hand that indicates it
-		VolumeToSound() //tells the volume and starts playing the music
-	}
-	drawLines()
+	// Validate all the necesary elements
+	drawTargetAreas(width, height)
 
+	if (setupReady()) {
+		drawKeypoints() //draw points in the hands
+		rightWrist.getTargetArea([targetArea1, targetArea2, targetArea3, targetArea4])
+	}
 	// drawSkeleton()
 }
 
-function drawLines() {
-	line(213, 0, 213, 580)
-	line(426, 0, 426, 580)
-	strokeWeight(4)
-	stroke(255, 204, 0)
+// DRAW FUNCTIONS
+function drawTargetAreas(canvasWidth, canvasHeight) {
+	targetArea1 = new TargetArea(canvasWidth / 4, canvasHeight / 4, 60, 60, 'green', 2)
+	targetArea2 = new TargetArea((canvasWidth / 4) * 3, canvasHeight / 4, 60, 60, '#EF8069', 1)
+	targetArea3 = new TargetArea(canvasWidth / 4, (canvasHeight / 4) * 3, 60, 60, '#EF8069', 4)
+	targetArea4 = new TargetArea((canvasWidth / 4) * 3, (canvasHeight / 4) * 3, 60, 60, '#EF8069', 3)
+	let targetAreas = [targetArea1, targetArea2, targetArea3, targetArea4]
+	targetAreas.forEach(targetArea => targetArea.display())
 }
+
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
 	// Loop through all the poses detected
-	if (leftWristPos.confidence > CONFIDENCE_THRESHOLD) {
-		fill(0, 255, 0, 100)
+	leftWrist.display()
+	rightWrist.display()
+}
 
-		//noStroke()
-		ellipse(leftWristPos.x, leftWristPos.y, 40, 40)
+function setHandDetection() {
+	if (rightWristPos.confidence > CONFIDENCE_THRESHOLD && leftWristPos.confidence > CONFIDENCE_THRESHOLD) {
+		handDetection = {
+			any: {
+				...handDetection.any,
+				isActive: false,
+			},
+			left: {
+				...handDetection.left,
+				isActive: true,
+			},
+			right: {
+				...handDetection.right,
+				isActive: true,
+			},
+		}
+	} else if (rightWristPos.confidence > CONFIDENCE_THRESHOLD && leftWristPos.confidence <= CONFIDENCE_THRESHOLD) {
+		handDetection = {
+			any: {
+				...handDetection.any,
+				isActive: false,
+			},
+			left: {
+				...handDetection.left,
+				isActive: false,
+			},
+			right: {
+				...handDetection.right,
+				isActive: true,
+			},
+		}
+	} else if (rightWristPos.confidence < CONFIDENCE_THRESHOLD && leftWristPos.confidence >= CONFIDENCE_THRESHOLD) {
+		handDetection = {
+			any: {
+				...handDetection.any,
+				isActive: false,
+			},
+			left: {
+				...handDetection.left,
+				isActive: true,
+			},
+			right: {
+				...handDetection.right,
+				isActive: false,
+			},
+		}
+	} else {
+		handDetection = {
+			any: {
+				...handDetection.any,
+				isActive: true,
+			},
+			left: {
+				...handDetection.left,
+				isActive: false,
+			},
+			right: {
+				...handDetection.right,
+				isActive: false,
+			},
+		}
 	}
-	if (rightWristPos.confidence > CONFIDENCE_THRESHOLD) {
-		fill(0, 0, 255, 100)
+}
 
-		//noStroke()
-		ellipse(rightWristPos.x, rightWristPos.y, 40, 40)
+function setWristActiveArea(wrist) {
+	let wristXCoord = wrist.getXCoord()
+	if (wrist.isActive()) {
 	}
 }
 
 function WhatInstrumentSounds() {
-	if (rightWristPos.confidence > CONFIDENCE_THRESHOLD && leftWristPos.confidence > CONFIDENCE_THRESHOLD) {
-		// l/r_hand 0 -> no instrument
-		// l/r_hand 1 -> piano
-		// l/r_hand 2 -> violin
-		// l/r_hand 3 -> guitar
-
-		if (rightWristPos.x <= 213) {
-			boolean_piano = 1
-			boolean_violin = 0
-			boolean_guitar = 0
-			r_hand = 1
-		} else if (rightWristPos.x <= 426) {
-			boolean_violin = 1
-			boolean_guitar = 0
-			boolean_piano = 0
-			r_hand = 2
-		} else if (rightWristPos.x <= 640) {
-			boolean_guitar = 1
-			boolean_violin = 0
-			boolean_piano = 0
-			r_hand = 3
+	// let { any, left, right } = handDetection
+	let instruments = [piano, guitar, violin]
+	if (!handDetection.any.isActive) {
+		if (handDetection.left.isActive) {
+			instruments.forEach(instrument => {
+				if (
+					handDetection.left.xCoord > instrument.threshhold.bottom &&
+					handDetection.left.xCoord <= instrument.threshhold.top
+				) {
+					handDetection.left = {
+						...handDetection.left,
+						instrument: instrument,
+						volume: getVolumeFromWrist(handDetection.left),
+					}
+				}
+			})
 		}
-
-		if (leftWristPos.x <= 213) {
-			boolean_piano = 1
-			l_hand = 1
-		} else if (leftWristPos.x <= 426) {
-			boolean_violin = 1
-			l_hand = 2
-		} else if (leftWristPos.x <= 640) {
-			boolean_guitar = 1
-			l_hand = 3
+		if (handDetection.right.isActive) {
+			instruments.forEach(instrument => {
+				if (
+					handDetection.right.xCoord > instrument.threshhold.bottom &&
+					handDetection.right.xCoord <= instrument.threshhold.top
+				) {
+					handDetection.right = {
+						...handDetection.right,
+						instrument: instrument,
+						volume: getVolumeFromWrist(handDetection.right),
+					}
+					playSong()
+				}
+			})
 		}
-	}
-
-	if (rightWristPos.confidence > CONFIDENCE_THRESHOLD && leftWristPos.confidence <= CONFIDENCE_THRESHOLD) {
-		l_hand = 0
-		if (rightWristPos.x <= 213.0) {
-			boolean_piano = 1
-			boolean_violin = 0
-			boolean_guitar = 0
-			r_hand = 1
-		} else if (rightWristPos.x <= 426.0) {
-			boolean_violin = 1
-			boolean_guitar = 0
-			boolean_piano = 0
-			r_hand = 2
-		} else if (rightWristPos.x <= 640.0) {
-			boolean_guitar = 1
-			boolean_violin = 0
-			boolean_piano = 0
-			r_hand = 3
-		}
-	}
-
-	if (leftWristPos.confidence > CONFIDENCE_THRESHOLD && rightWristPos.confidence <= CONFIDENCE_THRESHOLD) {
-		r_hand = 0
-
-		if (leftWristPos.x <= 213) {
-			boolean_piano = 1
-			boolean_violin = 0
-			boolean_guitar = 0
-			l_hand = 1
-		} else if (leftWristPos.x <= 426) {
-			boolean_violin = 1
-			boolean_guitar = 0
-			boolean_piano = 0
-			l_hand = 2
-		} else if (leftWristPos.x <= 640) {
-			boolean_guitar = 1
-			boolean_violin = 0
-			boolean_piano = 0
-			l_hand = 3
-		}
-	}
-
-	if (leftWristPos.confidence <= CONFIDENCE_THRESHOLD && rightWristPos.confidence <= CONFIDENCE_THRESHOLD) {
-		boolean_violin = 0
-		boolean_guitar = 0
-		boolean_piano = 0
-		r_hand = 0
-		l_hand = 0
+	} else {
+		handDetection.left.instrument = undefined
+		handDetection.right.instrument = undefined
 	}
 }
-//console.log('lw', 1-Math.round10(rightWristPos.y / height,-2))
-function VolumeToSound() {
-	volume_right = 1 - Math.round10(rightWristPos.y / height, -2)
-	if (volume_right < 0) {
-		volume_right = 0
-	} else if (volume_right > 1) {
-		volume_right = 1
-	}
 
-	volume_left = 1 - Math.round10(leftWristPos.y / height, -2)
-	if (volume_left < 0) {
-		volume_left = 0
-	} else if (volume_left > 1) {
-		volume_left = 1
-	}
+function VolumeToSound() {
+	volume_right = getVolumeFromWrist(rightWristPos)
+	volume_left = getVolumeFromWrist(leftWristPos)
 
 	if (boolean_piano == 1) {
 		if (r_hand == 1) {
@@ -313,6 +405,38 @@ function VolumeToSound() {
 	} else {
 		guitar.volume = 0
 	}
+}
+
+function getVolumeFromWrist(wrist) {
+	let volumeWrist
+
+	volumeWrist = 1 - Math.round10(wrist.yCoord / height, -2)
+
+	if (volumeWrist < 0) {
+		return 0
+	} else if (volumeWrist > 1) {
+		return 1
+	}
+
+	return volumeWrist
+}
+
+function playSong() {
+	if (handDetection.right.isActive) {
+		if (!handDetection.right.instrument.vals.isPlaying()) {
+			handDetection.right.instrument.vals.play()
+			handDetection.right.instrument.vals.setVolume(handDetection.right.volume)
+		}
+	} else if (!handDetection.right.isActive) {
+		handDetection.right.instrument.vals.pause()
+	}
+
+	// if (handDetection.left.isActive && !handDetection.left.instrument.vals.isPlaying()) {
+	// 	handDetection.left.instrument.vals.play()
+	// 	handDetection.left.instrument.vals.setVolume(handDetection.left.volume)
+	// } else {
+	// 	handDetection.right.instrument.vals.pause()
+	// }
 }
 
 ;(function () {
